2f6aaf636e1d86f678373a485be7b02255346a69
shyndick <3gggkostya@gmail.com>
2022-11-08 12:24:06 +0300

    css_update_08.11

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,36 @@ export const Footer = () => {
             <div className="container">
                 <div className="f_wrapper">
                     <div className="f_item">
-                    <NavLink to="/contacts">Контакты</NavLink>
-                    <NavLink to="/info">O компании</NavLink>
-                    <NavLink to="/delivery">Доставка и оплата</NavLink>
-                    <NavLink to="/politica">Политика</NavLink>
+                        <p className="f_title">Инфомация</p>
+                        <NavLink to="/contacts">Контакты</NavLink>
+                        <NavLink to="/info">O компании</NavLink>
+                        <NavLink to="/delivery">Доставка и оплата</NavLink>
+                        <NavLink to="/politica">Политика</NavLink>
                     </div>
                     <div className="f_item">
+                        <p className="f_title">Служба поддежки</p>
                         <NavLink to="/guarantee">Гарантия/Возврат</NavLink>
                         <NavLink to="/contacts">Обратная связь</NavLink>
                         <NavLink to="/map">Карта сайта</NavLink>
                     </div>
                     <div className="f_item">
+                        <p className="f_title">Покупателям</p>
                         <NavLink to="/catalog">Каталог</NavLink>
                         <NavLink to="/info">Услуги</NavLink>
                         <NavLink to="/delivery">Майнинг под ключ</NavLink>
                         <NavLink to="/politica">Сборка ферм</NavLink>
                     </div>
                     <div className="f_item">
-                        <a className='contact__email' href="email:maner@basa.com">miner@basa.com</a>
-                        <a className='contact__phone' href="tel:+375255275480">+375 25 527-54-80</a>
-                        <p>ООО"фйти Технологии"</p>
+                        <p className="f_title">Контактная инфомация</p>
+                        <a className='contact_email' href="email:maner@basa.com">miner@basa.com</a>
+                        <a className='contact_phone' href="tel:+375255275480">+375 25 527-54-80</a>
+                        <p>ООО"Aйти Технологии"</p>
                         <p>ИНН: 5465465465465</p>
+                        <div className="info_messege">
+                            <a href="https://www.instagram.com/" target='blank' className='massege_item'><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="https://web.telegram.org/" target='blank' className='massege_item'><i className="fa fa-telegram" aria-hidden="true"></i></a>
+                            <a href="https://www.whatsapp.com/" target='blank' className='massege_item'><i className="fa fa-whatsapp" aria-hidden="true"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import { NavLink } from "react-router-dom"
-import Viber from '../image/viberLogo.svg'
-import Telegram from '../image/telegramLogo.svg'
-import Instagram from '../image/instagramLogo.svg'
+import Logo  from "../image/logo.png"
 
 export const Header = () => {
     return (
@@ -10,26 +8,33 @@ export const Header = () => {
                 <div className="container"> 
                     <div className="info_wrapper">
                         <div className="info_items">
-                            <a className='info_email' href="email:maner@basa.com">miner@basa.com</a>
-                            <a className='info_phone' href="tel:+375255275480">+375 25 527-54-80</a>
-                            <a className='info_pozvoni' href="#">Заказать звонок</a>
+                            <a className='info_email' href="email:maner@basa.com"><i className="fa fa-envelope" aria-hidden="true"></i>miner@basa.com</a>
+                            <a className='info_phone' href="tel:+375255275480"><i className="fa fa-phone" aria-hidden="true"></i>+375 25 527-54-80</a>
+                            <a className='info_pozvoni' href="tel:+375255275480">Заказать звонок</a>
                         </div>
                         <div className="info_messege">
-                            <a href="https://www.viber.com/ru/" target='blank' className='massege_item'> <img src={Viber} alt="viber" /></a>
-                            <a href="https://web.telegram.org/" target='blank' className='massege_item'><img src={Telegram} alt="telegram" /></a>
-                            <a href="https://www.instagram.com/" target='blank' className='massege_item'><img src={Instagram} alt="instagram" /></a>
+                            <a href="https://www.instagram.com/" target='blank' className='massege_item'><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="https://web.telegram.org/" target='blank' className='massege_item'><i className="fa fa-telegram" aria-hidden="true"></i></a>
+                            <a href="https://www.whatsapp.com/" target='blank' className='massege_item'><i className="fa fa-whatsapp" aria-hidden="true"></i></a>
                         </div>
                     </div>    
                 </div>
             </div>
             <div className="container">
-                <div className='nav_wrapper'>
-                    <NavLink to="/">Главная</NavLink>
-                    <NavLink to="/catalog">Каталог</NavLink>
-                    <NavLink to="/guarantee">Гарантия/Возврат</NavLink>
-                    <NavLink to="/contacts">Контакты</NavLink>
-                    <NavLink to="/delivery">Доставка</NavLink>
-                    <NavLink to="/cart">Корзина</NavLink>
+                <div className="nav">
+                    <div className="logo">
+                        <a href="/"><img src={Logo} alt="" /></a>
+                    </div>
+                    <div className='nav_wrapper'>
+                        <NavLink className="nav_link" to="/">Главная</NavLink>
+                        <NavLink className="nav_link" to="/catalog">Каталог</NavLink>
+                        <NavLink className="nav_link" to="/guarantee">Гарантия/Возврат</NavLink>
+                        <NavLink className="nav_link" to="/contacts">Контакты</NavLink>
+                        <NavLink className="nav_link" to="/delivery">Доставка</NavLink>
+                    </div>
+                    <div className="nav_cart">
+                        <NavLink to="/cart"><i className="fa fa-shopping-cart" aria-hidden="true"></i></NavLink>
+                    </div>
                 </div>
             </div>
         </header>
